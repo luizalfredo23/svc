17a5556d1f6f1e1ea24b3257ecc4ee65d0ec6e99
Luiz Alfredo Carrara Bertusso <luizalfredocb@gmail.com>
2026-04-26 10:26:53 -0300

    Deleta arquivo

--- src/test/java/br/com/luizalfredo23/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package br.com.luizalfredo23;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
